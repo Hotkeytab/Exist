2f8592151b6947827fd9ba8be1b54ff27103888d
Taha C <taha.c@brainy-tn.com>
2022-03-30 17:37:47 +0100

    started kpi filters

---
+++ app/src/main/java/com/example/gtm/data/repository/ChartRepository.kt
@@ -0,0 +1,19 @@
+package com.example.gtm.data.repository
+
+import com.example.gtm.data.entities.remote.SignInPost
+import com.example.gtm.data.entities.ui.User
+import com.example.gtm.data.remote.BaseRemoteDataSource
+import com.example.gtm.data.remote.auth.AuthRemoteDataSource
+import com.example.gtm.data.remote.user.UserRemoteDataSource
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val userDataSource: UserRemoteDataSource
+) {
+
+    suspend fun getUser(username: String) = userDataSource.getUser(username)
+
+    suspend fun changeProfile(file: MultipartBody.Part?, user: RequestBody)= userDataSource.changeProfile(file,user)
+}
